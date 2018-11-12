631927928d2c33ac1261d6ab44e57cf073220f17
ROLAND <lejyend7890@ezweb.ne.jp>
2018-11-12 15:05:29 +0900

    ログイン画面追加

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head> 
+	<title>Login</title>
+</head>
+<body>
+
+<h1> ログイン </h1>
+
+<form action = "chat.php">
+	<input type = "text" name = "uname">
+	<button>ログイン</button>
+</form>
+
+</body>
+</html>
